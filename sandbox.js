9484bf8c0c26301cffcf210ab245dfbf9bbf39ba
Roshan Panigrahi <58463002+Roshan021198@users.noreply.github.com>
2020-04-23 21:57:58 +0530

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-const correctAnswer = ['A','A','C','A','B'];
+const correctAnswer = ['A','A','C','A','B','B','D','C','A','B'];
 const form = document.querySelector('.quiz-form');
 
 const  result = document.querySelector('.result');
@@ -7,8 +7,8 @@ form.addEventListener('submit',e => {
     e.preventDefault();
 
     let score = 0;
-userAnswer = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value]
-
+userAnswer = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value,form.q6.value,form.q7.value,form.q8.value,form.q9.value,form.q10.value]
+ 
     userAnswer.forEach((answer,index)=>{
         if(answer === correctAnswer[index]){
             score += 1
@@ -22,14 +22,56 @@ userAnswer = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.va
 
     let output = 0
     const timer = setInterval( () => {
-        result.querySelector('span').textContent = `${output}%`;
+        if(score >= 90)
+        {
+            result.querySelector('span').textContent = `${output}% 😍❤️ `;
+        }
+        else if(score >= 80 && score < 90)
+        {
+            result.querySelector('span').textContent = `${output}% 🥰` ;
+        }
+        else if(score >= 70 && score < 80)
+        {
+            result.querySelector('span').textContent = `${output}% 😉`;
+        }
+        else if(score >= 60 && score < 70)
+        {
+            result.querySelector('span').textContent = `${output}% 😙`;
+        }
+        else if(score >= 50 && score < 60)
+        {
+            result.querySelector('span').textContent = `${output}% 😗`;
+        }
+        else if(score >= 40 && score < 50)
+        {
+            result.querySelector('span').textContent = `${output}% 😑`;
+        }
+        else if(score >= 30 && score < 40)
+        {
+            result.querySelector('span').textContent = `${output}% 😨`;
+        }
+        else if(score >= 20 && score < 30)
+        {
+            result.querySelector('span').textContent = `${output}% 😵`;
+        }
+        else if(score >= 10 && score < 20)
+        {
+            result.querySelector('span').textContent = `${output}% 😡`;
+        }
+        else
+        {
+            result.querySelector('span').textContent = `${output}% 🤬`;
+        }
+
+//==================================================================================
+
         if(output === score){
             clearInterval(timer);
         }
         else{
             output++;
         }
-    },15);
+    },11);
 });
 
 
